034784d17202084259a7fca37e53bd8f592528b5
Harry Akeroyd <thirtythreezero@outlook.com>
2021-01-25 14:08:26 +1000

    Finished 25012021

--- Counting_Valleys_Hacker_Rank.py
+++ Counting_Valleys_Hacker_Rank.py
@@ -22,21 +22,21 @@ import sys
 
 #-----------------------------------------------------------------------------#
 
-
 def countingValleys(steps, path):
     # Write your code here
     up, down, valleys_walked = 0, 0, 0
 
     for step in path:
         if step == 'D': down += 1
+        if step == 'U': down -= 1
 
-        if down == 2:
-            valleys_walked += 1
+        if down == 0 and step == 'U': # We're coming out of a valley here
+            valleys_walked += 1       # therefore we have been in a valley
             down = 0
 
     return valleys_walked
 
 
-x = countingValleys(4, 'DUDUDD')
+x = countingValleys(12, 'DDUUDDUDUUUD')
 
 print(x)
