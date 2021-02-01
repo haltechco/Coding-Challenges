dbad7b1cfc2c7f68cd5e50db0da6d50f0639637a
Harry Akeroyd <thirtythreezero@outlook.com>
2021-02-02 08:44:10 +1000

    changed logic

--- Jumping_on_the_Clouds_Hacker_Rank.py
+++ Jumping_on_the_Clouds_Hacker_Rank.py
@@ -1,20 +1,21 @@
 # Complete the jumpingOnClouds function below.
 # Only concered about jumping from safe index to safe index.
 def jumpingOnClouds(c):
-
-    i=0
-    min_steps = 0
-    steps = c[1]
-
-    for i in list(steps):
-        if steps[i+2] == 0:
+    min_steps = i = 0
+    if c[0] == 1: return min_steps # One Cloud.
+    while i < (c[0]-1):
+        if c[1][i+2] == 0:
             min_steps += 1
-            print(steps[i+2])
+            i += 2
+        else:
+            min_steps += 1
+            i +=1
 
     return min_steps
 
-num_clouds = 7
-describing_clouds = [0,0,1,0,0,1,0]
+
+num_clouds = 3
+describing_clouds = [0,1,1]
 c = [num_clouds, describing_clouds]
 
 print(jumpingOnClouds(c))
