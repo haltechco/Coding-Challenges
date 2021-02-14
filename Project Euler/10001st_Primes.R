9f3848cca8c704a60c41ad3b95d1a16deabcd3ca
Harry Akeroyd <thirtythreezero@outlook.com>
2021-02-14 11:16:06 +1000

    Unfinished

---
+++ Project Euler/10001st_Primes.R
@@ -0,0 +1,20 @@
+# Project Euler Program:
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see 
+# that the 6th prime is 13.
+
+# Question: What is the 10,001st prime number?
+
+# A prime number is that, that is only divisible by itself and 1.
+
+# Create a list of prime numbers, once the list reaches 10,001, end. 
+# 10,001 prime is count.prime_number[-1]
+
+is.prime <- function(n){
+  
+}
+
+num_primes <- 0
+while(num_primes < 10001){
+  is.prime()
+  num_primes = num_primes + 1
+}
