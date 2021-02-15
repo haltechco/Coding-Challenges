99e6dade7a697742eee638a7b7c78087ede086fc
Harry Akeroyd <thirtythreezero@outlook.com>
2021-02-16 08:33:59 +1000

    finished largest product

---
+++ Project Euler/Largest_Product_in_a_Series.R
@@ -0,0 +1,43 @@
+# Project Euler:
+# The four adjacent digits in the 1000-digit number that have the greatest 
+# product are 9 × 9 × 8 × 9 = 5832.
+# E.g. 1 2 4 3 2
+#      3 5 1 ...
+# Find the thirteen adjacent digits in the 1000-digit number that have the 
+# greatest product. What is the value of this product?
+
+# Brute Force Method:
+# From position i, look ahead the next three positions and multiply, store,
+# then move to the next position and if larger, replace. Continue until end.
+
+data_set <- "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450"
+consec_length <- 4  # Define length of search.
+last_largest_num <- 0
+# We can only search up to where the adjacent numbers would reach the end of the list.
+len_searchable_data <- (nchar(data_set) - consec_length + 1)
+  
+# Logic:
+BFM <- function(consec_length){
+  for (i in 1:len_searchable_data) {
+    # Pick 'consec_length' of consecutive digits
+    adj_nums <- substr(data_set, i, i + consec_length - 1)
+    adj_nums <- as.numeric(unlist(strsplit(adj_nums, "")))
+    prod_consec <- prod(adj_nums)
+    # Replace if current largest number is greatest than last greatest.
+    if(prod_consec > last_largest_num){
+      last_largest_num <- prod_consec
+    }
+  }
+  return(last_largest_num)
+}
+
+cat("Largest product of ", 13, "consecutive numbers is: ", BFM(13))
+
+
+
+
+
+
+
+
+
