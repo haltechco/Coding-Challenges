d82d9a807d4954377d50baf0851e76ed35d5e9eb
Staley Street <n9997121@qut.edu.au>
2021-04-06 11:00:56 +1000

    All runs complete

---
+++ The HackerRank Interview Preperation Kit/Ransom_Note.py
@@ -0,0 +1,32 @@
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+# Complete the checkMagazine function below.
+def checkMagazine(magazine, note):
+
+    if len(note) > len(magazine): return print('No')
+
+    if Counter(note) - Counter(magazine) == {}:
+        print('Yes')
+    else:
+        print('No')
+
+    return None
+
+
+if __name__ == '__main__':
+    mn = input().split()
+
+    m = int(mn[0])
+
+    n = int(mn[1])
+
+    magazine = input().rstrip().split()
+
+    note = input().rstrip().split()
+
+    checkMagazine(magazine, note)
